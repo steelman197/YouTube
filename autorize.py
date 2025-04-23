36964cc9a9da36871ffbdef93624f0666b97140d
steelman197 <noil27@mail.ru>
2025-04-23 12:47:56 +0500

    Update autorize.py

--- autorize.py
+++ autorize.py
@@ -1,8 +1,8 @@
 from yoomoney import Authorize
 
 Authorize(
-    client_id="YOUR_CLIENT_ID",
-    redirect_uri="YOUR_REDIRECT_URI",
+    client_id="A7DED1A593309E04FF36D3BF817E21CEDF1E8C330F6BC715574E06D81D9D748A",
+    redirect_uri="https://t.me/my_first_VPN_bot",
     scope=["account-info",
            "operation-history",
            "operation-details",
